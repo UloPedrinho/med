ba18e529bfad243b9dbec391ea2246a97893afc2
pedro <pedro@montgomery.galiza>
2020-01-30 00:27:35 +0100

    * file.h: added loadFileInBuffer()

--- file.h
+++ file.h
@@ -1,9 +1,11 @@
 #ifndef _FILE_H
 #define _FILE_H
 
+#include <stdio.h>
 #include "buffer.h"
 
 int writeBufferToFile(Buffer, char [], char []);
 int readFileToBuffer(Buffer *, char [], char []);
+void loadFileInBuffer(Buffer *buffer, FILE *file);
 
 #endif
